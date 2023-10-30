83ceaf79e78b77aac178e116c200de2527f63804
malidegirmenci <m.alidegirmenci@hotmail.com>
2023-10-30 13:01:16 +0300

    object destructuring

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,14 +2,15 @@ import React from "react";
 import { ScProduct } from "./scParts";
 
 const Product = (props) => {
+  const {product, addItem} = props;
   return (
     <ScProduct>
-      <img src={props.product.image} alt={`${props.product.title} book`} />
+      <img src={product.image} alt={`${product.title} book`} />
       <div className="details">
-        <h1 className="title">{props.product.title}</h1>
+        <h1 className="title">{product.title}</h1>
         <div className="footer">
-          <p className="price">${props.product.price}</p>
-          <button onClick={() => props.addItem(props.product)}>
+          <p className="price">${product.price}</p>
+          <button onClick={() => addItem(product)}>
             Add to cart
           </button>
         </div>
