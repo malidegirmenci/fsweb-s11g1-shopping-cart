c38094d44d46ce78308a83b0a93988a9384f5200
malidegirmenci <m.alidegirmenci@hotmail.com>
2023-10-30 16:11:14 +0300

    destructuring provider s

--- src/App.js
+++ src/App.js
@@ -1,66 +1,30 @@
-import React, { useEffect, useState } from "react";
 import { Route } from "react-router-dom";
-import { data } from "./data";
 
 // Bileşenler
 import Navigation from "./components/Navigation";
 import Products from "./components/Products";
 import ShoppingCart from "./components/ShoppingCart";
-import { ProductContext } from "./contexts/ProductContext";
-import { CartContext } from "./contexts/CartContext";
 
+import { CartProvider } from "./contexts/CartContext";
+import { ProductProvider } from "./contexts/ProductContext";
 function App() {
-  const [products, setProducts] = useState(data);
-  const [cart, setCart] = useState([]);
-  const LSKey = "ItemsInCart";
-  useEffect(() => {
-    setCart(getDataFromLS());
-  },[])
-
-  function writeFavsToLocalStorage(state) {
-    localStorage.setItem(LSKey, JSON.stringify(state));
-  }
-  function readFavsFromLocalStorage() {
-    return JSON.parse(localStorage.getItem(LSKey));
-  }
-  function getDataFromLS() {
-    if(readFavsFromLocalStorage()){
-      return readFavsFromLocalStorage();
-    }else {
-      return cart;
-    }
-  }
-  const addItem = (item) => {
-    setCart([...cart, item])
-    writeFavsToLocalStorage([...cart, item])
-  };
-
-  
-  const removeItem = (item) => {
-    const remainingItems = cart.filter((i) => i.id !== item.id)
-    setCart(remainingItems);
-    writeFavsToLocalStorage(remainingItems)
-  }
-
   return (
-    <ProductContext.Provider value={{ products, addItem, removeItem }}>
+    <ProductProvider >
       <div className="App">
-        <CartContext.Provider value={{ cart }}>
+        <CartProvider >
           <Navigation />
-        </CartContext.Provider>
-        {/* Routelar */}
-        <main className="content">
-          <Route exact path="/">
-            <Products />
-          </Route>
-          <CartContext.Provider value={{ cart }}>
+          {/* Routelar */}
+          <main className="content">
+            <Route exact path="/">
+              <Products />
+            </Route>
             <Route path="/cart">
               <ShoppingCart />
             </Route>
-          </CartContext.Provider>
-        </main>
+          </main>
+        </CartProvider>
       </div>
-    </ProductContext.Provider>
+    </ProductProvider>
   );
 }
 
