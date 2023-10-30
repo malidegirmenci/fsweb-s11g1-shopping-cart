b82da9b443e7fc13899a27edcbd06da41a6bd385
malidegirmenci <m.alidegirmenci@hotmail.com>
2023-10-30 12:58:55 +0300

    Context structure imported

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import { data } from "./data";
 import Navigation from "./components/Navigation";
 import Products from "./components/Products";
 import ShoppingCart from "./components/ShoppingCart";
+import { ProductContext } from "./contexts/ProductContext";
+import { CartContext } from "./contexts/CartContext";
 
 function App() {
   const [products, setProducts] = useState(data);
@@ -16,20 +18,24 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <Navigation cart={cart} />
-
-      {/* Routelar */}
-      <main className="content">
-        <Route exact path="/">
-          <Products products={products} addItem={addItem} />
-        </Route>
-
-        <Route path="/cart">
-          <ShoppingCart cart={cart} />
-        </Route>
-      </main>
-    </div>
+    <ProductContext.Provider value={{ products, addItem }}>
+      <div className="App">
+        <CartContext.Provider value={{cart}}>
+          <Navigation />
+        </CartContext.Provider>
+        {/* Routelar */}
+        <main className="content">
+          <Route exact path="/">
+            <Products products={products} addItem={addItem} />
+          </Route>
+          <CartContext.Provider value={{cart}}>
+            <Route path="/cart">
+              <ShoppingCart/>
+            </Route>
+          </CartContext.Provider>
+        </main>
+      </div>
+    </ProductContext.Provider>
   );
 }
 
