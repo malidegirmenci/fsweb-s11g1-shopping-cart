1bf0ec132f5bd652dfd2868991ab9cf1ffc452e8
malidegirmenci <m.alidegirmenci@hotmail.com>
2023-10-30 16:12:28 +0300

    refactor destructuring ProductContext

--- src/components/Products.js
+++ src/components/Products.js
@@ -20,11 +20,12 @@ const ScProducts = styled.section`
 `;
 
 const Products = () => {
-  const { products, addItem } = useContext(ProductContext);
+  const { products } = useContext(ProductContext);
+  
   return (
     <ScProducts>
       {products.map((product) => (
-        <Product key={product.id} product={product} addItem={addItem} />
+        <Product key={product.id} product={product} />
       ))}
     </ScProducts>
   );
