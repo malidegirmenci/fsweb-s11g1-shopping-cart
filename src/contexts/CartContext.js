f6c53d2c8a253f10e843fdde8e0c2dd30a8d0edf
malidegirmenci <m.alidegirmenci@hotmail.com>
2023-10-30 16:13:06 +0300

    created CartProvider

--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -1,2 +1,42 @@
-import { createContext } from 'react';
-export  const CartContext = createContext();
+import { createContext, useEffect, useState } from 'react';
+export const CartContext = createContext();
+
+export const CartProvider = ({ children }) => {
+    const LSKey = "ItemsInCart";
+    const [cart, setCart] = useState([]);
+
+    useEffect(() => {
+        setCart(getDataFromLS());
+    }, [])
+
+    function writeFavsToLocalStorage(state) {
+        localStorage.setItem(LSKey, JSON.stringify(state));
+    }
+    function readFavsFromLocalStorage() {
+        return JSON.parse(localStorage.getItem(LSKey));
+    }
+    function getDataFromLS() {
+        if (readFavsFromLocalStorage()) {
+            return readFavsFromLocalStorage();
+        } else {
+            return cart;
+        }
+    }
+    const addItem = (item) => {
+        setCart([...cart, item])
+        writeFavsToLocalStorage([...cart, item])
+    };
+
+
+    const removeItem = (item) => {
+        const remainingItems = cart.filter((i) => i.id !== item.id)
+        setCart(remainingItems);
+        writeFavsToLocalStorage(remainingItems)
+    }
+
+    return (
+        <CartContext.Provider value={{ cart, setCart, addItem, removeItem }}>
+            {children}
+        </CartContext.Provider>
+    )
+}
