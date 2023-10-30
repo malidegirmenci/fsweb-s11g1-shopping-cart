b8828ddae12af2b59bfcfa6ed8641ca975f100f5
malidegirmenci <m.alidegirmenci@hotmail.com>
2023-10-30 16:13:19 +0300

    created ProductProvider

--- src/contexts/ProductContext.js
+++ src/contexts/ProductContext.js
@@ -1,2 +1,14 @@
-import { createContext } from 'react';
-export  const ProductContext = createContext();
+import { createContext, useState } from 'react';
+
+import { data } from "../data";
+
+export const ProductContext = createContext();
+
+export const ProductProvider = ({ children }) => {
+    const [products, setProducts] = useState(data);
+    return(
+        <ProductContext.Provider value={{products}}>
+            {children}
+        </ProductContext.Provider>
+    )
+}
